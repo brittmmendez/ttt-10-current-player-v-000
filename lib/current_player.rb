6de2d5d1f1d18e74e8c052dac9ac13d659d6f355
Brittany Morris <brittmmendez@gmail.com>
2017-11-10 21:15:24 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,9 @@
   def turn_count(board)
    count = 0
-   board.each do |turn|
-     if turn == "X" || turn == "O"
+   board.each do |mark|
+     if mark == "X" || mark == "O"
         count += 1
       end
     end
-
+    puts count
   end
