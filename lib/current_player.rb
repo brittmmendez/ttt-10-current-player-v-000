e26eab341342f95098d0e2dca487da15feac3e25
Brittany Morris <brittmmendez@gmail.com>
2017-11-10 21:11:30 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,11 @@
+  def turn_count(board)
+   count = 0
+   board.each do |turn|
+     if turn == "X" || turn == "O"
+        count += 1
+      end
+end
+def current_player(board)
+  if count%2==0 
+
+end
