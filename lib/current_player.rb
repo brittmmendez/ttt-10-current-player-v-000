01b6dec237e8067260189d3718f86aea173d759a
Brittany Morris <brittmmendez@gmail.com>
2017-11-10 21:29:23 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -7,3 +7,16 @@
     end
     return counter
   end
+
+  def current_player(board)
+    play_even=["First","Third","Fifth","Seventh","Ninth"]
+    play_odd=["Second","Fourth","Sixth","Eigth"]
+
+    if turn_count(board)%2==0?
+     play_even.each do |play_x|
+           puts "The correct player, X, for the #{play_x} move"
+    else
+      play_odd.each do |play_o|
+        puts "The correct player, O, for the #{play_o} move"
+    end
+  end
